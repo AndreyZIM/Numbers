8f4a8eaafa10584e372a1c46eddcbc7f1f6e6dc5
Andrey <zaxarenkoandrey@gmail.com>
2022-11-17 10:54:24 +0300

    fix MainActivity cast to ProvideViewModel

--- app/src/main/java/com/github/andreyzim/numbers/main/presentation/MainActivity.kt
+++ app/src/main/java/com/github/andreyzim/numbers/main/presentation/MainActivity.kt
@@ -3,10 +3,14 @@ package com.github.andreyzim.numbers.main.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
 import com.github.andreyzim.numbers.R
+import com.github.andreyzim.numbers.main.sl.ProvideViewModel
 import com.github.andreyzim.numbers.numbers.presentation.NumbersFragment
 
-class MainActivity : AppCompatActivity(), ShowFragment {
+class MainActivity : AppCompatActivity(), ShowFragment, ProvideViewModel {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,4 +24,7 @@ class MainActivity : AppCompatActivity(), ShowFragment {
 
     override fun show(fragment: Fragment) =
         NavigationStrategy.Add(fragment).navigate(supportFragmentManager, R.id.container)
+
+    override fun <T : ViewModel> provideViewModel(clazz: Class<T>, owner: ViewModelStoreOwner): T =
+        (application as ProvideViewModel).provideViewModel(clazz, owner)
 }
